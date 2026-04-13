bbb0f084f9398b6b96881160cd19a174252190cc
sergioqfeg1 <sergioqfeg@gmail.com>
2026-04-13 16:36:23 +0200

    Primeros pasos con la autenticacion de Clerk

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,12 +1,19 @@
-import { Stack } from "expo-router";
-import { StatusBar } from "expo-status-bar";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { Slot } from "expo-router";
+import { ClerkProvider } from '@clerk/expo'
+import { tokenCache } from '@clerk/expo/token-cache'
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY
+
+if (!publishableKey) {
+  throw new Error('Add your Clerk Publishable Key to the .env file')
+}
 
 export default function RootLayout() {
   return (
-    <SafeAreaView style={{flex: 1}}>
-      <Stack screenOptions={{headerShown: false}} />
-      <StatusBar style="dark" />
-    </SafeAreaView>
+    <ClerkProvider
+      publishableKey={publishableKey!}
+      tokenCache={tokenCache}>
+      <Slot />
+    </ClerkProvider>
   );
 }
