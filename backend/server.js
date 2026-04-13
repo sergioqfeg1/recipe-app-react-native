a1ecd6776341ccda609c96932d76f86d78b4be23
sergioqfeg1 <sergioqfeg@gmail.com>
2026-04-13 12:50:22 +0200

    Pasado el puerto a un .env

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 import express from "express"
+import "dotenv/config"
 
 const app = express()
+const port = process.env.PORT || 8001
 
-app.listen(5001, () => {
-    console.log('Server is running on http://localhost:5001')
+app.listen(port, () => {
+    //Al correr con nodemon se actualiza automaticamente (dev)
+    console.log('Server is running on http://localhost:',port)
 })
